da496503c2f396e52ba029f9ab0a540e63960b23
mohaubai <mubaid675@gmail.com>
2026-09-22 15:32:16 +0000

    updated refunds_day.sh file with mart command and error checks

--- refunds_day.sh
+++ refunds_day.sh
@@ -2,7 +2,7 @@
 
 set -e
 DAY=$1
-part = $(echo $DAY | tr -d '-')
+PART=$(echo $DAY | tr -d '-')
 
 #generate file
 python3 gen_refunds.py $DAY
@@ -14,7 +14,7 @@ gcloud storage cp refunds_$DAY.csv gs://payflow_ubaid_09/refunds/raw/
 bq load --source_format=CSV --replace --skip_leading_rows=1 \
 "learning-gcp-929:payflow_raw.refunds\$$PART" \
 gs://payflow_ubaid_09/refunds/raw/refunds_$DAY.csv \
-txn_id:STRING,customer_id:STRING,amount:NUMERIC,currency:STRING,created_at:TIMESTAMP
+refund_id:STRING,txn_id:STRING,refund_amount:NUMERIC,refund_reason:STRING,created_at:TIMESTAMP
 
-
-
+#update biquery marts dataset with daily_net_revenue table
+bq query --use_legacy_sql=false < daily_net_revenue.sql
